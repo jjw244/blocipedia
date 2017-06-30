10d0cbe0f2d1d72e974e7d0203abc2e9417169d6
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-30 16:04:37 -0500

    Added Event tracking on sign-up (devise and manual)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,18 @@ $(document).on('turbolinks:load', function () {
     var html = converter.makeHtml(text);
     $('#wiki_preview').html(html);
   });
+
+  $('.user_sign_up').click(function() {
+    console.log("testing");
+    blocmetrics.report("user signed up");
+  });
 });
+
+var blocmetrics = {};
+blocmetrics.report = function(eventName){
+  var event = { event: { name: eventName }};
+  var request = new XMLHttpRequest();
+  request.open("POST", "https://blocmetrics-jw.herokuapp.com/api/events", true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(event));
+};
